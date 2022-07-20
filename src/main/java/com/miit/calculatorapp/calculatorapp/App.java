737284dc094feb8d361f1112f8c5efc70aa317dd
pruthugujarati <gujaratipruthavi600@gmail.com>
2022-07-19 21:17:15 -0400

    third commit

--- src/main/java/com/miit/calculatorapp/calculatorapp/App.java
+++ src/main/java/com/miit/calculatorapp/calculatorapp/App.java
@@ -2,86 +2,11 @@ package com.miit.calculatorapp.calculatorapp;
 
 import java.util.Scanner;
 
-public class App 
+class App1
 {
-    public static void main( String[] args )
-    {
-       int i=0;
-		
-		while(i<5) {
-		System.out.println("Select Performance: ");
-		System.out.println("1: Additon ");
-		System.out.println("2: Substraction ");
-		System.out.println("3: Multipication");
-		System.out.println("4: Division ");
-		System.out.println("5: Exit ");
-		
-		Scanner performance = new Scanner(System.in);
-		System.out.println("Enter performance number here:");
-		i = performance.nextInt();
-		
-		switch (i) {
-		case 1:
-			int x, y, sum;
-			Scanner myObj = new Scanner(System.in);
-			System.out.println("Enter Value");
-			x = myObj.nextInt();
-			
-			System.out.println("Enter another Value");
-	        y = myObj.nextInt();
-	        
-	        addition(x, y);
-	        System.out.println("Addition ans is :" + (x + y));
-			break;
-		
-		case 2:
-			int sub;
-			Scanner myObj1 = new Scanner(System.in);
-			System.out.println("Enter Value");
-			x = myObj1.nextInt();
-			
-			System.out.println("Enter another Value");
-	        y = myObj1.nextInt();
-	        
-	        substraction(x, y);
-	        System.out.println("Substraction ans is :" + (x -y));
-			break;
-			
-		case 3:
-			
-			int mul;
-			Scanner myObj2 = new Scanner(System.in);
-			System.out.println("Enter Value");
-			x = myObj2.nextInt();
-			
-			System.out.println("Enter another Value");
-	        y = myObj2.nextInt();
-	        
-	        multiply(x, y);
-	        System.out.println("Multipication ans is :" + (x * y));
-			break;
-			
-		case 4:
-			float div;
-			Scanner myObj3 = new Scanner(System.in);
-			System.out.println("Enter Value");
-			x = myObj3.nextInt();
-			
-			System.out.println("Enter another Value");
-	        y = myObj3.nextInt();
-	        
-	        division(x, y);
-	        System.out.println("Division ans is :" +  (x /y));
-			break;
-		}
-		}
-	}
 
-	public static int multiply(int x, int y) {
-		
-		return x * y;
-	}
-
+	App1() {}
+	
 	public static int addition(int x, int y) {
 		
 		return x + y;
@@ -91,11 +16,129 @@ public class App
 		
 		return x - y;
 	}
+	
+    public static int multiply(int x, int y) {
+		
+		return x * y;
+	}
 
-	public static float division(int x, int y) {
+	public static int division(int x, int y) {
 		
 		return x / y;
 	}
 
+	public static int square(int x, int y) {
+		
+		return x * x;
+	}
+
+	public static int math(int x, int y) {
+		return Math.max(x, y);
+	}
+    
+        
+		}
+	public class App{
+		public static void main( String[] args ) {
+			int i=0;
+			
+			while(i<7) {
+			System.out.println("Select Performance: ");
+			System.out.println("1: Additon ");
+			System.out.println("2: Substraction ");
+			System.out.println("3: Multipication");
+			System.out.println("4: Division ");
+			System.out.println("5: Square ");
+			System.out.println("6: Find Maximum Number ");
+			System.out.println("7: Exit ");
+			
+			Scanner performance = new Scanner(System.in);
+			System.out.println("Enter performance number here:");
+			i = performance.nextInt();
+			int x = 0, y=0;
+			App1 obj = new App1();
+			int result=0;
+			switch (i) {
+			case 1:
+				
+				Scanner myObj = new Scanner(System.in);
+				System.out.println("Enter Value = ");
+				x = myObj.nextInt();
+				
+				System.out.println("Enter another Value = ");
+		        y = myObj.nextInt();
+		        
+		       result= obj.addition(x, y);
+		        
+				System.out.println("Addition ans is :" + result);
+				break;
+			
+			case 2:
+				
+				Scanner myObj1 = new Scanner(System.in);
+				System.out.println("Enter Value = ");
+				x = myObj1.nextInt();
+				
+				System.out.println("Enter another Value = ");
+		        y = myObj1.nextInt();
+		        
+		        result =obj.substraction(x, y);
+		        System.out.println("Substraction ans is :" + result);
+				break;
+				
+			case 3:
+				
+				
+				Scanner myObj2 = new Scanner(System.in);
+				System.out.println("Enter Value = ");
+				x = myObj2.nextInt();
+				
+				System.out.println("Enter another Value = ");
+		        y = myObj2.nextInt();
+		        
+		        result = obj.multiply(x, y);
+		        System.out.println("Multipication ans is :" + result);
+				break;
+				
+			case 4:
+				
+				Scanner myObj3 = new Scanner(System.in);
+				System.out.println("Enter Value = ");
+				x = myObj3.nextInt();
+				
+				System.out.println("Enter another Value = ");
+		        y = myObj3.nextInt();
+		        
+		        result =obj.division(x, y);
+		        System.out.println("Division ans is :" +  result);
+				break;
+				
+	         case 5:
+				
+				Scanner myObj4 = new Scanner(System.in);
+				System.out.println("Enter Value = ");
+				x = myObj4.nextInt();
+		        
+		        result = obj.square(x,x);
+		        System.out.println("Square is :" + result);
+				break;
+				
+	         case 6:
+	 			
+	 			Scanner myObj5 = new Scanner(System.in);
+	 			System.out.println("Enter Value = ");
+	 			x = myObj5.nextInt();
+	 			
+	 			System.out.println("Enter another Value = ");
+	 	        y = myObj5.nextInt();
+	 	        
+	 	        result = Math.max(x, y);
+	 	        System.out.println("Maximum number is :" + result);
+	 			break;
+	 			default:
+	 				System.out.println("Unvalid entry");
+			}
+	    }
+	}
  }
 
--- src/test/java/com/miit/calculatorapp/calculatorapp/testCalculator.java
+++ src/test/java/com/miit/calculatorapp/calculatorapp/testCalculator.java
@@ -8,25 +8,41 @@ public class testCalculator {
 	@Test
 	
 	public void testadd() {
-		assertEquals(9, 5+4);
+		App1 cal = new App1();
+		assertEquals(9, cal.addition(5, 4));
 	}
 
    @Test
 	
 	public void testsub() {
-		assertEquals(1, 5-4);
+	   App1 cal = new App1();
+		assertEquals(1, cal.substraction(5, 4));
 	}
    
    @Test
 
     public void testmul() {
-	assertEquals(20, 5*4);
+	   App1 cal = new App1();
+	assertEquals(20, cal.multiply(5,4));
 }
    
    @Test
 
    public void testdiv() {
-	assertEquals(3, 12/4);
+	   App1 cal = new App1();
+	assertEquals(4, cal.division(12, 3));
 }
+   @Test
+   
+   public void testsquare() {
+	   App1 cal = new App1();
+	   assertEquals(25, cal.square(5, 5));
+   }
+   
+   @Test
+   public void testmath() {
+	   App1 cal = new App1();
+	   assertEquals(7, cal.math(7, 5));
+   }
 }
  
